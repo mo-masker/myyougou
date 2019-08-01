81cbf5de574f1ca3626550e6d6dfa1ff6e402ef3
mo <105025127@qq.com>
2019-08-01 16:49:49 +0800

    首页结构

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,69 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 1.轮播图数组
+    swiperList:[],
+    // 2.分类导航数组
+    navCateList:[],
+    // 3.楼层数组
+    floorList:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onLoad(){
+    this.getSwiperList();
+    this.getNavCateList();
+    this.getFloorList();
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+
+  // 获取轮播图数据
+  getSwiperList(){
+    // 1.发送请求 获取数据
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      // data: {},
+      // header: {'content-type':'application/json'},
+      // method: 'GET',
+      // dataType: 'json',
+      // responseType: 'text',
+      success: (result) => {
+        // console.log(result);
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          swiperList:result.data.message
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+      },
+      // fail: () => {},
+      // complete: () => {}
+    });
+      
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  // 导航分类
+  getNavCateList(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+        console.log(result);
+        this.setData({
+          navCateList:result.data.message
+        })
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+      
+  },
+
+  // 获取楼层数据
+  getFloorList(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success: (result) => {
+        this.setData({
+          floorList:result.data.message
+        })
+      },
+      
+    });
+      
   }
+ 
 })
